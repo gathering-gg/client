0627c87dfd500e1e26dece0c729967279fa95d83
Ethan Mick <ethan@ethanmick.com>
2019-01-24 21:56:46 -0500

    Add GitHub token to get around auth limitations

--- .travis/fetch_binary.sh
+++ .travis/fetch_binary.sh
@@ -2,6 +2,7 @@
 OS=$1
 URL=$(curl \
   -s \
+  -u "ethanmick:$GITHUB_TOKEN" \
   -H 'Accept: application/vnd.github.v3+json' \
   -H 'User-Agent: gathering.gg-ci' \
   https://api.github.com/repos/gathering-gg/parser/releases/latest \
@@ -9,10 +10,14 @@ URL=$(curl \
 )
 
 if [[ $OS == "windows" ]]; then
-  curl -s -o "resources/$OS/gathering.exe" $URL
+  curl -s -o "resources/$OS/gathering.exe" \
+    -u "ethanmick:$GITHUB_TOKEN" \
+    $URL
   chmod +x "resources/$OS/gathering.exe"
 else 
-  curl -s -o "resources/$OS/gathering" $URL
+  curl -s -o "resources/$OS/gathering" \
+    -u "ethanmick:$GITHUB_TOKEN" \
+    $URL
   chmod +x "resources/$OS/gathering"
 fi
 
